4b74ccd161ecfc2b3c8862341d0889bd4f0ed772
nnovic <nnovic@yahoo.fr>
2017-06-28 16:11:46 +0200

    initialisation d'une classe de test

---
+++ GetFactsTests/Facts/FactsTests.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GetFacts.Facts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GetFacts.Download;
+using System.IO;
+
+namespace GetFacts.Facts.Tests
+{
+    [TestClass()]
+    public class FactsTests
+    {
+        private class TestableFacts:Facts
+        {
+            public TestableFacts():base("RotationTestConfig.json")
+            {
+
+            }
+        }
+
+        private class TestableDownloadManager: DownloadManager
+        {
+            public TestableDownloadManager():base()
+            {
+                uniqueInstance = this;
+            }
+
+            protected override bool DownloadsFileSupported
+            {
+                get { return false; }
+            }
+        }
+
+        private class TestableDownloadTask:DownloadTask
+        {
+            public TestableDownloadTask(string file, string id):base(  
+                new Uri(
+                    Path.Combine("Cache",file), 
+                    UriKind.Relative), 
+                Guid.Parse(id) )
+            {
+            }
+        }
+
+
+        /// <summary>
+        /// Test of the Facts#More() method.
+        /// </summary>
+        [TestMethod()]
+        public void MoreTest()
+        {
+            Facts facts = new TestableFacts();
+            DownloadManager dm = new TestableDownloadManager();
+            dm.Queue( new TestableDownloadTask("site1.html", "00000000-0000-0000-0000-000000000001") );
+            Assert.Fail();
+        }
+    }
+}
