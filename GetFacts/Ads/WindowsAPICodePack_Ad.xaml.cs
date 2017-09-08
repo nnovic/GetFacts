9162fb3efc7e259221aca498951eab6ee6395e21
nnovic <nnovic@yahoo.fr>
2017-09-08 10:19:28 +0200

    ajoute "pub" pour WindowsAPICodePack

---
+++ GetFacts/Ads/WindowsAPICodePack_Ad.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace GetFacts.Ads
+{
+    /// <summary>
+    /// Logique d'interaction pour WindowsAPICodePack_Ad.xaml
+    /// </summary>
+    public partial class WindowsAPICodePack_Ad : UserControl
+    {
+        public static readonly string URL = "http://html-agility-pack.net/";
+
+        public WindowsAPICodePack_Ad()
+        {
+            InitializeComponent();
+        }
+
+        private void UserControl_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            Process.Start(URL);
+        }
+    }
+}
