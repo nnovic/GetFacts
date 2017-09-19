795d30a5aabd15128323dc177a2017fc0de7ce14
nnovic <nnovic@yahoo.fr>
2017-09-19 13:35:54 +0200

    modif de Refresh

--- GetFacts/Facts/PageConfig.cs
+++ GetFacts/Facts/PageConfig.cs
@@ -11,7 +11,6 @@ namespace GetFacts.Facts
     {
         public PageConfig()
         {
-            Refresh = 60;
             Enabled = true;
         }
 
@@ -30,14 +29,63 @@ namespace GetFacts.Facts
             get; set;
         }
 
+        #region Refresh
+
+        private int refresh = DefaultRefresh;
+
         /// <summary>
-        /// (en minutes)
+        /// La plus petite période de rafraîchissement que
+        /// l'utilisateur peut configurer est 10 minutes
+        /// (valeur exprimée en minutes).
         /// </summary>
+        /// <see cref="Refresh"/>
+        public const int MinRefresh = 10;
+
+        /// <summary>
+        /// La plus grande période de rafraîchissement
+        /// que l'utilisateur peut configurer est 4 heures
+        /// (valeur exprimée en minutes, soit 240).
+        /// </summary>
+        /// <see cref="Refresh"/>
+        public const int MaxRefresh = 4 * 60;
+
+        /// <summary>
+        /// valeur initialie du rafraîchissement (valeur
+        /// exprimée en minutes).
+        /// </summary>
+        /// <see cref="Refresh"/>
+        public const int DefaultRefresh = 60;
+
+        /// <summary>
+        /// Définit, en minutes, l'intervalle de temps entre deux mises à jour
+        /// de la page. Cette valeur doit être comprise
+        /// entre MinRefresh et MaxRefresh.
+        /// </summary>
+        /// <see cref="MinRefresh"/>
+        /// <see cref="MaxRefresh"/>
         [DefaultValue(60)]
         public int Refresh
         {
-            get; set;
+            get
+            {
+                if (refresh < MinRefresh) return DefaultRefresh;
+                if (refresh > MaxRefresh) return DefaultRefresh;
+                return refresh;
+            }
+            set
+            {
+                if( (value<MinRefresh) || (value>MaxRefresh) )
+                {
+                    throw new ArgumentOutOfRangeException("Refresh");
+                }
+                else
+                {
+                    refresh = value;
+                }
+            }
         }
+
+        #endregion
 
         [DefaultValue(true)]
         public bool Enabled
