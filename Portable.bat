9474179fff3a02553fa40ae8629efd5936a45e16
nnovic <nnovic@yahoo.fr>
2017-10-31 16:45:31 +0100

    inclure TemplatesApp dans la version "portable"

--- Portable.bat
+++ Portable.bat
@@ -1,4 +1,5 @@
 set zipfile=GetFacts_0.2_Portable.zip
 del .\Output\%zipfile%
 .\7-Zip\7za.exe a -x!GetFactsApp.exe -x!*.xml -x!*.pdb .\Output\%zipfile% .\GetFactsApp\bin\Debug\*
+.\7-Zip\7za.exe a                    -x!*.xml -x!*.pdb .\Output\%zipfile% .\TemplatesApp\bin\Debug\*
 pause
